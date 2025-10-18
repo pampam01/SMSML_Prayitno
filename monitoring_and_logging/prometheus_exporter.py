d8cc58a81fb20530b03ed1520ed22be90e85ff5e
pampam01 <emailhpwiko@gmail.com>
2025-10-18 09:14:44 +0700

    change something

--- monitoring_and_logging/prometheus_exporter.py
+++ monitoring_and_logging/prometheus_exporter.py
@@ -2,23 +2,22 @@ from prometheus_client import start_http_server, Gauge
 import time
 import random
 
-# Buat tiga metrik berbeda untuk dipantau
+
 REQUEST_COUNT = Gauge('total_requests', 'Total jumlah request prediksi')
 PREDICTION_CONFIDENCE = Gauge('prediction_confidence_score', 'Rata-rata skor kepercayaan prediksi')
 ERROR_RATE = Gauge('prediction_error_rate', 'Tingkat kesalahan prediksi')
 
 def generate_metrics():
     """Fungsi sederhana untuk mensimulasikan perubahan metrik."""
-    REQUEST_COUNT.inc(random.randint(1, 5)) # Simulasikan ada request baru
-    PREDICTION_CONFIDENCE.set(random.uniform(0.75, 0.99)) # Simulasikan skor kepercayaan
-    ERROR_RATE.set(random.uniform(0.01, 0.05)) # Simulasikan tingkat error yang rendah
+    REQUEST_COUNT.inc(random.randint(1, 5)) 
+    PREDICTION_CONFIDENCE.set(random.uniform(0.75, 0.99)) 
+    ERROR_RATE.set(random.uniform(0.01, 0.05)) 
 
 if __name__ == '__main__':
-    # Mulai server HTTP untuk mengekspos metrik di port 8000
     start_http_server(8000)
     print("Prometheus exporter berjalan di http://localhost:8000")
 
-    # Terus menerus menghasilkan nilai metrik baru
+ 
     while True:
         generate_metrics()
-        time.sleep(5) # Update setiap 5 detik
+        time.sleep(5) 
